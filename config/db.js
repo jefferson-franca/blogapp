63bab7d3edd67f228d98bad66c8a9154dd382bb8
Jefferson França <jefferson-franca@hotmail.com>
2022-03-20 14:59:21 -0300

    make it better

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI:"mongodb+srv://jeffsf:iEdeNXLeLN00aj1J@cluster0.iwm6x.mongodb.net/Cluster0?retryWrites=true&w=majority"}
+    module.exports = {mongoURI:"mongodb+srv://jeffsf:iEdeNXLeLN00aj1J@cluster0.iwm6x.mongodb.net/Cluster0retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI:"mongodb://localhost/blogapp"}
 }
